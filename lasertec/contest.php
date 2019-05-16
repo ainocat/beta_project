678c33cbb890d3f911c87756ebfb546316f4b18a
Ainocat <ainocat@outlook.com>
2019-05-17 01:00:16 +0800

    show with table

--- lasertec/contest.php
+++ lasertec/contest.php
@@ -4,13 +4,43 @@
 <meta charset="utf-8">
 <title>test connect</title>
 <?php
-$servername="localhost";
+//establish connection with PDO
+$dbms="mysql";
+$host="localhost";
+$dbName="ch13";
 $username="root";
 $password="lasertec";
+$dsn="$dbms:host=$host;dbname=$dbName";
 
 try{
-    $conn=new PDO("mysql:host=$servername;",$username,$password);
-    echo "connect successful";
+    $conn=new PDO($dsn,$username,$password);
+    //echo "connect successful";
+    //read all data wit foreach
+    /*
+    $result=$conn->query('SELECT * from parts');
+    foreach($result as $row){
+        print_r($row[0]);
+        echo "<br />";
+    }
+    */
+    //use prepare method if sql is wrong, will catch error.
+    //the benefit of prepare is format or multi var. 
+    $sth = $conn->prepare('SELECT * from parts');
+    $sth->execute();
+    //give fetch style.
+    /*
+    while($result=$sth->fetch(PDO::FETCH_NUM))
+    {
+        foreach($result as $row)
+        {
+            print_r($row);
+            echo "  ";
+            }//end foreach
+        echo "<br />";
+        }//end while
+
+    $conn=null;
+    */
 }catch(PDOException $e){
     echo $e->getMessage();
 }
@@ -19,6 +49,36 @@ try{
 </head>
 
 <body>
+<table border="1" cellspacing="1" cellpadding="1">
+    <tr>
+        <th width="200" scope="col">Part Number</th>
+        <th width="200" scope="col">Name</th>
+        <th width="50" scope="col">SH</th>
+        <th width="50" scope="col">NJ</th>
+        <th width="50" scope="col">WX</th>
+        <th width="50" scope="col">Total</th>
+    </tr>
+    <?php
+        while($result=$sth->fetch(PDO::FETCH_NUM))
+        {
+    ?>
+    <tr>
+    <?php
+            foreach($result as $row)
+            {
+    ?>
+        <td>&nbsp;<?php echo $row; ?></td>
+    <?php
+            }//end foreach
+        }//end while
+    ?>
+    </tr>
+
+</table>
 
 </body>
 </html>
+<?php
+    
+    $conn=null;
+?>
